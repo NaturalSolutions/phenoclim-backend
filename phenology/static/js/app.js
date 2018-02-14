660f4877f0798e8e6a6e3721e6bbe2cbde783540
cdcvidal <cdcvidal@yahoo.fr>
2018-02-14 18:20:31 +0100

    Fix error: geojson featuresCollection

--- phenology/static/js/app.js
+++ phenology/static/js/app.js
@@ -101,7 +101,7 @@ phenoclim.map = function(options){
       var bounds = self.geojson.getBounds();
       self._map.fitBounds(bounds, { maxZoom: 18, padding: [10, 10] });
       //DEFAULT AREA/Create
-      if(options.geojson.features[0].coordinates[0] == 4 && options.geojson.features[0].coordinates[1] == 44)
+      if(!options.geojson.features[0].features && options.geojson.features[0].coordinates[0] == 4 && options.geojson.features[0].coordinates[1] == 44)
         self._map.fitBounds(bounds2, { maxZoom: 18, padding: [10, 10] });
     }
     else{
